68ff94647398c10db3df26193f8785866ea70c1a
Joost de Valk <joost@yoast.com>
2016-03-12 23:08:00 +0100

    Refactor output into several functions for ease of reading

--- classes/class-design.php
+++ classes/class-design.php
@@ -31,31 +31,10 @@ if ( ! class_exists( 'YoastSEO_AMP_Design' ) ) {
 		 * Add additional CSS to the AMP output
 		 */
 		public function additional_css() {
-
 			require 'views/additional-css.php';
 
-			$css_builder = new YoastSEO_AMP_CSS_Builder();
-			$css_builder->add_option( 'header-color', 'nav.amp-wp-title-bar', 'background' );
-			$css_builder->add_option( 'headings-color', '.amp-wp-title, h2, h3, h4', 'color' );
-			$css_builder->add_option( 'text-color', '.amp-wp-content', 'color' );
-
-			$css_builder->add_option( 'blockquote-bg-color', '.amp-wp-content blockquote', 'background-color' );
-			$css_builder->add_option( 'blockquote-border-color', '.amp-wp-content blockquote', 'border-color' );
-			$css_builder->add_option( 'blockquote-text-color', '.amp-wp-content blockquote', 'color' );
-
-			$css_builder->add_option( 'link-color', 'a, a:active, a:visited', 'color' );
-			$css_builder->add_option( 'link-color-hover', 'a:hover, a:focus', 'color' );
-
-			$css_builder->add_option( 'meta-color', '.amp-wp-meta li, .amp-wp-meta li a', 'color' );
-
-			echo $css_builder->build();
-
-			if ( ! empty( $this->options['extra-css'] ) ) {
-				$safe_text = strip_tags($this->options['extra-css']);
-				$safe_text = wp_check_invalid_utf8( $safe_text );
-				$safe_text = _wp_specialchars( $safe_text, ENT_NOQUOTES );
-				echo $safe_text;
-			}
+			$this->css_builder();
+			$this->extra_css();
 		}
 
 		/**
@@ -73,6 +52,39 @@ if ( ! class_exists( 'YoastSEO_AMP_Design' ) ) {
 			do_action( 'wpseo_opengraph' );
 
 			echo strip_tags( $this->options['extra-head'], '<link><meta>' );
+		}
+
+		/**
+		 * Build the CSS
+		 */
+		private function css_builder() {
+			$css_builder = new YoastSEO_AMP_CSS_Builder();
+			$css_builder->add_option( 'header-color', 'nav.amp-wp-title-bar', 'background' );
+			$css_builder->add_option( 'headings-color', '.amp-wp-title, h2, h3, h4', 'color' );
+			$css_builder->add_option( 'text-color', '.amp-wp-content', 'color' );
+
+			$css_builder->add_option( 'blockquote-bg-color', '.amp-wp-content blockquote', 'background-color' );
+			$css_builder->add_option( 'blockquote-border-color', '.amp-wp-content blockquote', 'border-color' );
+			$css_builder->add_option( 'blockquote-text-color', '.amp-wp-content blockquote', 'color' );
+
+			$css_builder->add_option( 'link-color', 'a, a:active, a:visited', 'color' );
+			$css_builder->add_option( 'link-color-hover', 'a:hover, a:focus', 'color' );
+
+			$css_builder->add_option( 'meta-color', '.amp-wp-meta li, .amp-wp-meta li a', 'color' );
+
+			echo $css_builder->build();
+		}
+
+		/**
+		 * Outputs the extra-css option after some sanitation
+		 */
+		private function extra_css() {
+			if ( ! empty( $this->options['extra-css'] ) ) {
+				$safe_text = strip_tags($this->options['extra-css']);
+				$safe_text = wp_check_invalid_utf8( $safe_text );
+				$safe_text = _wp_specialchars( $safe_text, ENT_NOQUOTES );
+				echo $safe_text;
+			}
 		}
 	}
 }
